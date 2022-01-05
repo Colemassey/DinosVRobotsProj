22a199329bf3875d68d06d70109a426704c05938
colemassey <cole.massey33@gmail.com>
2022-01-05 14:12:25 -0600

    added while loop to battle method in battlefield

--- robodinoproj/battlefield.py
+++ robodinoproj/battlefield.py
@@ -9,7 +9,7 @@ class Battlefield:
 
     def run_game(self): # use run_game to call your other methods you create in battlefield
         self.display_welcome()
-        self.robot_turn()
+        self.battle()
         pass
 
     def display_welcome(self):
@@ -17,18 +17,38 @@ class Battlefield:
         pass
     
 
-    def battle():
+    def battle(self):
+        game_end = False
+        round_counter = 1
+        while game_end == False:
+            if self.herd.dinos.health or self.fleet.robots.health >= 1:
+                self.robot_turn()
+                self.dino_turn()
+                print(f"Round {round_counter} is over")
+                round_counter = +1
+                return
+            
+            elif self.herd.dinos.health or self.fleet.robots.health <= 0:
+                game_end == True
+                self.show_winners()
+                break
+
         pass
 
     def dino_turn(self):
+        self.show_dino_options()
+        dino_index = int(input("Select a dinosaur to attack with"))
+        self.robot_show_options()
+        robot_index = int(input("Select a robot to attack"))
+        self.herd.dinos[dino_index].attack(self.fleet.robots[robot_index])
         pass
 
     def robot_turn(self):
         self.robot_show_options()
-        robo_index = int(input("Select a robot to attack with"))
+        robot_index = int(input("Select a robot to attack with"))
         self.show_dino_options()
         dino_index = int(input("Select a dino to attack"))
-        self.fleet.robots[robo_index].attack(self.herd.dinos[dino_index])
+        self.fleet.robots[robot_index].attack(self.herd.dinos[dino_index])
         pass
 
     def show_dino_options(self):
@@ -48,6 +68,7 @@ class Battlefield:
         pass
 
     def show_winners(self):
+        print(f"battle over")
         pass
 
     # finish UML from battlefield list
